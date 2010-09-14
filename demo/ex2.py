a9dee198068030b3a7f0433b1cc2a50503aa2a72
Lisandro Dalcin <dalcinl@gmail.com>
2010-09-14 12:22:25 -0300

    Fix ex2 and ex3 to solve the same eigenvalue problem

--- demo/ex2.py
+++ demo/ex2.py
@@ -4,6 +4,35 @@ slepc4py.init(sys.argv)
 from petsc4py import PETSc
 from slepc4py import SLEPc
 
+Print = PETSc.Sys.Print
+
+def construct_operator(m, n):
+    """
+    Standard symmetric eigenproblem corresponding to the
+    Laplacian operator in 2 dimensions.
+    """
+    # Create matrix for 2D Laplacian operator
+    A = PETSc.Mat().create()
+    A.setSizes([m*n, m*n])
+    A.setFromOptions( )
+    # Fill matrix
+    hx = 1.0/(m-1) # x grid spacing
+    hy = 1.0/(n-1) # y grid spacing
+    diagv = 2.0*hy/hx + 2.0*hx/hy
+    offdx = -1.0*hy/hx
+    offdy = -1.0*hx/hy
+    Istart, Iend = A.getOwnershipRange()
+    for I in xrange(Istart, Iend) :
+        A[I,I] = diagv
+        i = I//n    # map row number to
+        j = I - i*n # grid coordinates
+        if i> 0  : J = I-n; A[I,J] = offdx
+        if i< m-1: J = I+n; A[I,J] = offdx
+        if j> 0  : J = I-1; A[I,J] = offdy
+        if j< n-1: J = I+1; A[I,J] = offdy
+    A.assemble()
+    return A
+
 def solve_eigensystem(A, problem_type=SLEPc.EPS.ProblemType.HEP):
     # Create the results vectors
     xr, tmp = A.getVecs()
@@ -18,57 +47,39 @@ def solve_eigensystem(A, problem_type=SLEPc.EPS.ProblemType.HEP):
     # Solve the eigensystem
     E.solve()
 
-    print
+    Print("")
     its = E.getIterationNumber()
-    print "Number of iterations of the method: %i" % its
+    Print("Number of iterations of the method: %i" % its)
     sol_type = E.getType()
-    print "Solution method: %s" % sol_type
+    Print("Solution method: %s" % sol_type)
     nev, ncv, mpd = E.getDimensions()
-    print "Number of requested eigenvalues: %i" % nev
+    Print("Number of requested eigenvalues: %i" % nev)
     tol, maxit = E.getTolerances()
-    print "Stopping condition: tol=%.4g, maxit=%d" % (tol, maxit)
+    Print("Stopping condition: tol=%.4g, maxit=%d" % (tol, maxit))
     nconv = E.getConverged()
-    print "Number of converged eigenpairs: %d" % nconv
+    Print("Number of converged eigenpairs: %d" % nconv)
     if nconv > 0:
-        print
-        print "        k          ||Ax-kx||/||kx|| "
-        print "----------------- ------------------"
+        Print("")
+        Print("        k          ||Ax-kx||/||kx|| ")
+        Print("----------------- ------------------")
         for i in range(nconv):
             k = E.getEigenpair(i, xr, xi)
             error = E.computeRelativeError(i)
-            if k.imag != 0.0: print " %9f%+9f j %12g" % (k.real, k.imag, error)
-            else: print " %12f       %12g" % (k.real, error)
-        print
+            if k.imag != 0.0:
+              Print(" %9f%+9f j  %12g" % (k.real, k.imag, error))
+            else:
+              Print(" %12f       %12g" % (k.real, error))
+        Print("")
 
-def construct_operator():
-    """
-    Standard symmetric eigenproblem corresponding to the
-    Laplacian operator in 2 dimensions.
-    """
-    # Problem size
+def main():
     opts = PETSc.Options()
-    m = n = opts.getInt('N', 40)
-    # Create matrix for Laplacian operator
-    A = PETSc.Mat().create()
-    A.setSizes([n*m, n*m])
-    A.setFromOptions( )
-    # Fill matrix
-    Istart, Iend = A.getOwnershipRange()
-    for I in range(Istart,Iend):
-        v = -1.0; i = I/n; j = I-i*n;
-        if i>0:
-            J=I-n; A[I,J] = v
-        if i<m-1:
-            J=I+n; A[I,J] = v
-        if j>0:
-            J=I-1; A[I,J] = v
-        if j<n-1:
-            J=I+1; A[I,J] = v
-        v=4.0; A[I,I] = v
-    A.assemble()
-    return A
-
-if __name__ == '__main__':
-    A = construct_operator()
+    N = opts.getInt('N', 32)
+    m = opts.getInt('m', N)
+    n = opts.getInt('n', m)
+    Print("Symmetric Eigenproblem (sparse matrix), "
+          "N=%d (%dx%d grid)" % (m*n, m, n))
+    A = construct_operator(m,n)
     solve_eigensystem(A)
 
+if __name__ == '__main__':
+    main()
--- demo/ex3.py
+++ demo/ex3.py
@@ -3,6 +3,50 @@ slepc4py.init(sys.argv)
 
 from petsc4py import PETSc
 from slepc4py import SLEPc
+import numpy as np
+
+Print = PETSc.Sys.Print
+
+def laplace2d(U, x, f):
+    U[:,:] = 0
+    U[1:-1, 1:-1] = x
+    # Grid spacing
+    m, n = x.shape
+    hx = 1.0/(m-1) # x grid spacing
+    hy = 1.0/(n-1) # y grid spacing
+    # Setup 5-points stencil
+    u  = U[1:-1, 1:-1] # center
+    uN = U[1:-1,  :-2] # north
+    uS = U[1:-1, 2:  ] # south
+    uW = U[ :-2, 1:-1] # west
+    uE = U[2:,   1:-1] # east
+    # Apply Laplacian
+    f[:,:] = \
+         (2*u - uE - uW) * (hy/hx) \
+       + (2*u - uN - uS) * (hx/hy) \
+
+class Laplacian2D(object):
+
+    def __init__(self, m, n):
+        self.m, self.n = m, n
+        scalar = PETSc.ScalarType
+        self.U = np.zeros([m+2, n+2], dtype=scalar)
+
+    def mult(self, A, x, y):
+        m, n = self.m, self.n
+        xx = x[...].reshape(m,n)
+        yy = y[...].reshape(m,n)
+        laplace2d(self.U, xx, yy)
+
+def construct_operator(m, n):
+    """
+    Standard symmetric eigenproblem corresponding to the
+    Laplacian operator in 2 dimensions. Uses *shell* matrix.
+    """
+    # Create shell matrix
+    context = Laplacian2D(m,n)
+    A = PETSc.Mat().createPython([m*n,m*n], context)
+    return A
 
 def solve_eigensystem(A, problem_type=SLEPc.EPS.ProblemType.HEP):
     # Create the results vectors
@@ -17,73 +61,40 @@ def solve_eigensystem(A, problem_type=SLEPc.EPS.ProblemType.HEP):
 
     # Solve the eigensystem
     E.solve()
-    print
+    Print("")
     its = E.getIterationNumber()
-    print "Number of iterations of the method: %i" % its
+    Print("Number of iterations of the method: %i" % its)
     sol_type = E.getType()
-    print "Solution method: %s" % sol_type
+    Print("Solution method: %s" % sol_type)
     nev, ncv, mpd = E.getDimensions()
-    print "Number of requested eigenvalues: %i" % nev
+    Print("Number of requested eigenvalues: %i" % nev)
     tol, maxit = E.getTolerances()
-    print "Stopping condition: tol=%.4g, maxit=%d" % (tol, maxit)
+    Print("Stopping condition: tol=%.4g, maxit=%d" % (tol, maxit))
     nconv = E.getConverged()
-    print "Number of converged eigenpairs: %d" % nconv
+    Print("Number of converged eigenpairs: %d" % nconv)
     if nconv > 0:
-        print
-        print "        k          ||Ax-kx||/||kx|| "
-        print "----------------- ------------------"
+        Print("")
+        Print("        k          ||Ax-kx||/||kx|| ")
+        Print("----------------- ------------------")
         for i in range(nconv):
             k = E.getEigenpair(i, xr, xi)
             error = E.computeRelativeError(i)
-            if k.imag != 0.0: print " %9f%+9f j %12g" % (k.real, k.imag, error)
-            else: print " %12f       %12g" % (k.real, error)
-        print
+            if k.imag != 0.0:
+              Print(" %9f%+9f j  %12g" % (k.real, k.imag, error))
+            else:
+              Print(" %12f       %12g" % (k.real, error))
+        Print("")
 
 
-def laplace2d(x, f):
-    # setup 5-points stencil
-    u  = x[1:-1, 1:-1] # center
-    uN = x[1:-1,  :-2] # north
-    uS = x[1:-1, 2:  ] # south
-    uW = x[ :-2, 1:-1] # west
-    uE = x[2:,   1:-1] # east
-    # apply Laplacian
-    nx, ny = x.shape
-    hx = 1.0/(nx-1) # x grid spacing
-    hy = 1.0/(ny-1) # y grid spacing
-    f[:,:] = x
-    f[1:-1, 1:-1] = \
-         (2*u - uE - uW) * (hy/hx) \
-       + (2*u - uN - uS) * (hx/hy) \
-
-
-class Laplacian2D(object):
-
-    def __init__(self, M, N):
-        self.M = M
-        self.N = N
-
-    def mult(self, A, x, y):
-        m = self.N
-        n = self.N
-        xx = x[...].reshape(m,n)
-        yy = y[...].reshape(m,n)
-        laplace2d(xx, yy)
-
-
-def construct_operator():
-    """
-    Standard symmetric eigenproblem corresponding to the
-    Laplacian operator in 2 dimensions. Uses *shell* matrix.
-    """
-    # Problem size
+def main():
     opts = PETSc.Options()
-    m = n = opts.getInt('N', 40)
-    # Create shell matrix
-    context = Laplacian2D(m,n)
-    A = PETSc.Mat().createPython([m*n,m*n], context)
-    return A
+    N = opts.getInt('N', 32)
+    m = opts.getInt('m', N)
+    n = opts.getInt('n', m)
+    Print("Symmetric Eigenproblem (matrix-free), "
+          "N=%d (%dx%d grid)" % (m*n, m, n))
+    A = construct_operator(m,n)
+    solve_eigensystem(A)
 
 if __name__ == '__main__':
-    A = construct_operator()
-    solve_eigensystem(A)
+    main()
