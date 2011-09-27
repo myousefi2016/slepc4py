54b62007c98b3924e5d70f6489eb394b05de5cee
Lisandro Dalcin <dalcinl@gmail.com>
2011-09-27 11:43:29 -0300

    Bump version number to 1.2

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ a large, sparse, rectangular matrix.
 """
 
 __author__    = 'Lisandro Dalcin'
-__version__   = '1.1'
+__version__   = '1.2'
 __credits__   = 'SLEPc Team <slepc-maint@grycap.upv.es>'
 
 # -----------------------------------------------------------------------------
